ec0ad6dc385d95f61bf493949525a29d4fe42c73
zseszti0 <113005557+zseszti0@users.noreply.github.com>
2025-03-21 20:02:07 +0100

    Add files via upload

---
+++ allapotgep.cpp
@@ -0,0 +1,164 @@
+/**
+ * \file allapotgep.cpp
+ *
+ * Ebben a fájlban kell megvalósítania az Allapotgep osztály
+ * metódusait, valamint mindazon további osztályokat, melyek szükségesek
+ * a feladat megvalósításához.
+ *
+ * Ezt a fájlt be kell adni (fel kell tölteni) a megoldással.
+ */
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+#include "allapotgep.h"
+#include "memtrace.h"
+
+///KONFIG KEZELÉS LOGIKA
+///
+
+Configuration::Configuration(const char *filename){
+
+    //Getting the file
+    //
+    std::ifstream file(filename);
+
+
+    if (!file.is_open()) {
+        throw "N6R1CW";
+    }
+    //Getting the number of states
+    //
+    char numberOfStates[2];
+    file.getline(numberOfStates, 2);
+    size = atoi(numberOfStates);
+
+    //Filling up the state information
+    //
+    stateNameTable = new char*[size];
+    for (int i = 0; i < size; ++i) {
+        stateNameTable[i] = new char[21];  // 20 characters + 1 for '\0'
+    }
+    stateValidityTable = new bool[size];
+
+    for (int i = 0; i < size; i++) {
+        stateValidityTable[i] = ((char)file.get() == 'I' ? true : false);
+        file.get();
+        file.getline(stateNameTable[i], 21);
+    }
+
+    //Filling the stateTable
+    //
+    stateTable = new char**[size];
+    for (int i = 0; i < size; ++i) {
+        stateTable[i] = new char*[size];
+        for (int j = 0; j < size; ++j) {
+            stateTable[i][j] = new char[5];
+        }
+    }
+
+    char lineBuffer[1024];
+    for (int i = 0; i < size; ++i) {
+        file.getline(lineBuffer,1023);
+        std::istringstream stream(lineBuffer);
+
+        for (int j = 0; j < size; ++j)
+            stream >> stateTable[i][j];
+
+    }
+
+    file.close();
+}
+int Configuration::StateNameToIndex(const char* stateName) {
+    for (int i = 0; i < size; i++) {
+        if (!strcmp(stateName, stateNameTable[i]))
+            return i;
+    }
+    return -1;
+}
+bool Configuration::IsStateValid(const char* stateName) {
+    return stateValidityTable[this->StateNameToIndex(stateName)];
+}
+char* Configuration::NextState(const char* currentState, Bazis inputBase) {
+    int stateIndex = this->StateNameToIndex(currentState);
+    char baseToSearch = cast(inputBase);
+    for (int i = 0; i < size; i++) {
+        if(strchr(stateTable[stateIndex][i],baseToSearch) != NULL) {
+            return stateNameTable[i];
+        }
+    }
+    return NULL;
+}
+Configuration& Configuration::operator=(const Configuration& rhs) {
+    if (this != &rhs) {
+        size = rhs.size;
+        stateNameTable = new char*[size];
+        for (int i = 0; i < size; i++) {
+            stateNameTable[i] = new char[21];
+            strcpy(stateNameTable[i], rhs.stateNameTable[i]);
+        }
+        stateValidityTable = new bool[size];
+        for (int i = 0; i < size; i++) {
+            stateValidityTable[i] = rhs.stateValidityTable[i];
+        }
+
+        stateTable = new char**[size];
+        for (int i = 0; i < size; i++) {
+            stateTable[i] = new char*[size];
+            for (int j = 0; j < size; j++) {
+                stateTable[i][j] = new char[5];
+                strcpy(stateTable[i][j], rhs.stateTable[i][j]);
+            }
+        }
+    }
+    return *this;
+}
+Configuration::~Configuration() {
+    for (int i = 0; i < size; i++) {
+        delete[] stateNameTable[i];
+    }
+    delete[] stateNameTable;
+    delete[] stateValidityTable;
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            delete[] stateTable[i][j];
+        }
+        delete[] stateTable[i];
+    }
+    delete[] stateTable;
+}
+
+
+///ÁLLAPOTGÉP LOGIKA
+///
+
+void Allapotgep::konfigural(const char *fajlnev) {
+    config = Configuration(fajlnev);
+
+    strcpy(currentState,config.GetBaseState());
+    isValid = config.IsStateValid(currentState);
+
+}
+const char *Allapotgep::aktualisallapot() {
+    return currentState;
+}
+bool Allapotgep::elfogad() {
+    return isValid;
+}
+void Allapotgep::atmenet(Bazis b) {
+    char* nextState = config.NextState(currentState, b);
+    strcpy(currentState, nextState);
+    isValid = config.IsStateValid(currentState);
+}
+bool Allapotgep::feldolgoz(const Bazis *b, int n) {
+    for (int i = 0; i < n; i++) {
+        this->atmenet(b[i]);
+    }
+    return isValid;
+}
+void Allapotgep::alaphelyzet() {
+    strcpy(currentState,config.GetBaseState());
+}
+Allapotgep::~Allapotgep() = default;
---
+++ genetika_teszt.cpp
@@ -0,0 +1,86 @@
+/**
+ * \file genetika_teszt.cpp
+ *
+ * Genetika nevű szorgalmi feladat tesztjei gtest_lite eszközkészletével megvalósítva.
+ * A szorgalmi feladat megoldásához ezt az állományt nem kell beadni (feltölteni).
+ *
+ * A ELKESZULT makró vezérli az egyes tesztesetek fordítását, ezzel lehetővé válik
+ * hogy kisebb lépésekben teszteljünk.
+ * Kezdetben ez 0, azaz nem tesztel semmit!
+ *
+ */
+
+#include <iostream>
+
+#include "gtest_lite.h"
+#include "allapotgep.h"
+#include "memtrace.h"
+
+#define ELKESZULT 3
+
+int main() {
+    GTINIT(std::cin); // Csak C(J)PORTA működéséhez kell
+    #if ELKESZULT > 0
+    /// Azt vizsgálja, hogy dob-e hibát a fájl megnyitásakor
+        TEST(Konfig, KonfigHiba) {
+            Allapotgep gep;
+            try {
+                EXPECT_THROW_THROW(gep.konfigural("nincs_ilyen_fajl"), const char* p);
+            } catch (const char *p) {
+            #ifdef CPORTA
+                EXPECT_ENVCASEEQ("ORG_ID", p);
+            #endif
+            }
+        } END
+    #endif // ELKESZULT > 0
+
+    #if ELKESZULT > 1
+    /// Azt vizsgálja, hogy be tudja-e olvasni a konfigurációs fájlt
+        TEST(ParosTrimin, fajbolkonfig1) {
+            Allapotgep gep;
+            EXPECT_NO_THROW(gep.konfigural("paros.txt"));
+            EXPECT_STREQ("ParosTimin", gep.aktualisallapot());
+        } END
+    #endif // ELKESZULT > 1
+
+    #if ELKESZULT > 1
+    /// Kipróbálja az egyik pédakonfigot
+        TEST(ParosTrimin, fajbolkonfig1) {
+            Allapotgep gep;
+            EXPECT_NO_THROW(gep.konfigural("paros.txt"));
+            EXPECT_STREQ("ParosTimin", gep.aktualisallapot());
+            Bazis demo[] = {Timin, Citozin, Guanin, Adenin, Timin};
+            EXPECT_EQ(true, gep.feldolgoz(demo, 5));
+            gep.alaphelyzet();
+            EXPECT_STREQ("ParosTimin", gep.aktualisallapot());
+            EXPECT_EQ(true, gep.elfogad());
+            EXPECT_EQ(false, gep.feldolgoz(demo, 4));
+        } END
+    #endif
+
+    #if ELKESZULT > 2
+    /// Kipróbálja az másik pédakonfigot
+        TEST(Implikacio: AA->CC, fajbolkonfig2) {
+            Allapotgep gep;
+            EXPECT_NO_THROW(gep.konfigural("implikacio.txt"));
+            EXPECT_STREQ("Alap", gep.aktualisallapot());
+            Bazis demo[] = {Adenin, Adenin, Timin, Citozin, Adenin, Citozin, Citozin, Guanin};
+            EXPECT_EQ(true, gep.feldolgoz(demo, 8));
+            gep.alaphelyzet();
+            gep.atmenet(Citozin);
+            EXPECT_STREQ("C1", gep.aktualisallapot());
+            gep.atmenet(Adenin);
+            gep.atmenet(Timin);
+            EXPECT_STREQ("A1", gep.aktualisallapot());
+            Bazis test[] = {Citozin, Citozin, Timin, Adenin, Guanin, Citozin};
+            gep.feldolgoz(test, 6);
+            EXPECT_STREQ("C2", gep.aktualisallapot());
+        } END
+    #endif
+
+    /// Itt a vége
+
+    GTEND(std::cerr); // Csak C(J)PORTA működéséhez kell
+    return 0;
+}
+
